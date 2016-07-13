bcdc5467919e77c2d4257a7414e7fe8821cef6da
William L. Marr III <hobbyist@wlmiii.net>
2016-07-13 17:20:35 -0400

    Add source file for the Brain class to the project

---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,13 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by William L. Marr III on 7/13/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import Foundation
+
+class Brain: NSObject {
+
+}
