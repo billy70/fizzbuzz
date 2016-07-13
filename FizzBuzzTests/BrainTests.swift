923d0124c18ca3310034b4f391812b8033042504
William L. Marr III <hobbyist@wlmiii.net>
2016-07-13 17:17:31 -0400

    Create first failing test
    The failure is due to the class Brain not existing yet. This is the first step in TDD; create a test that fails because the class being tested does not even exist yet.

--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -20,16 +20,9 @@ class BrainTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(3)
+        XCTAssertEqual(result, true)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
