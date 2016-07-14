fd473e35ecef1b44c70495a86f4eb93b7c610553
William L. Marr III <hobbyist@wlmiii.net>
2016-07-14 13:12:27 -0400

    Add unit test class GameTests to test the game engine

---
+++ FizzBuzzTests/GameTests.swift
@@ -0,0 +1,23 @@
+//
+//  GameTests.swift
+//  FizzBuzz
+//
+//  Created by William L. Marr III on 7/14/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import XCTest
+
+class GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
