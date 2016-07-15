59b536bb684f5335d545b125159e704190f54d01
William L. Marr III <hobbyist@wlmiii.net>
2016-07-15 11:49:48 -0400

    Add unit test for testing interaction between view and model

---
+++ FizzBuzzTests/ViewControllerUnitTests.swift
@@ -0,0 +1,36 @@
+//
+//  ViewControllerUnitTests.swift
+//  FizzBuzz
+//
+//  Created by William L. Marr III on 7/15/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class ViewControllerUnitTests: XCTestCase {
+    
+    var viewController: ViewController!
+    
+    override func setUp() {
+        super.setUp()
+
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        viewController = storyboard.instantiateViewControllerWithIdentifier("ViewController") as! ViewController
+        UIApplication.sharedApplication().keyWindow!.rootViewController = viewController
+        
+        let _ = viewController.view
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func test() {
+        XCTAssertTrue(true)
+    }
+    
+}
