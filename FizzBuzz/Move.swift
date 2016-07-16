3dedda77fb7dc7cb32df041c1f87e0c7add5eb01
William L. Marr III <hobbyist@wlmiii.net>
2016-07-16 11:16:17 -0400

    Add enumeration for the four possible moves in the game

---
+++ FizzBuzz/Move.swift
@@ -0,0 +1,16 @@
+//
+//  Move.swift
+//  FizzBuzz
+//
+//  Created by William L. Marr III on 7/16/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import Foundation
+
+enum Move {
+    case Number
+    case Fizz
+    case Buzz
+    case FizzBuzz
+}
